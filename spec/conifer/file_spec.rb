16af1470e6471569067354309f51b80b83749ee3
Hamed Asghari <hasghari@g2.com>
2020-02-26 09:52:49 -0700

    Add specs

---
+++ spec/conifer/file_spec.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+require 'conifer/file'
+
+RSpec.describe Conifer::File do
+  subject(:file) { described_class.new(name, dir: dir, prefix: prefix) }
+
+  let(:name) { :foo }
+  let(:dir) { File.expand_path(__dir__) }
+  let(:prefix) { nil }
+
+  describe '#path' do
+    context 'when file is in current directory' do
+      it 'returns path to file' do
+        expect(file.path).to match 'spec/conifer/foo.yml'
+      end
+    end
+
+    context 'when file is in other directory' do
+      let(:dir) { File.expand_path('../config', __dir__) }
+
+      it 'returns path to file' do
+        expect(file.path).to match 'spec/config/foo.yml'
+      end
+    end
+
+    context 'when file is in parent directory' do
+      let(:name) { :bar }
+      let(:dir) { File.expand_path('..', __dir__) }
+
+      it 'returns path to file' do
+        expect(file.path).to match 'spec/bar.yml'
+      end
+    end
+  end
+
+  describe '#exists?' do
+    context 'when file exists' do
+      it 'returns true' do
+        expect(file.exists?).to eq true
+      end
+    end
+
+    context 'when file does not exist' do
+      let(:name) { :missing }
+
+      it 'returns false' do
+        expect(file.exists?).to eq false
+      end
+    end
+  end
+
+  describe '#validate!' do
+    context 'when file exists' do
+      it 'does not raise error' do
+        expect { file.validate! }.not_to raise_error
+      end
+    end
+
+    context 'when file does not exist' do
+      let(:name) { :missing }
+
+      it 'raises error' do
+        expect { file.validate! }.to raise_error Conifer::File::NotFoundError
+      end
+    end
+  end
+
+  describe '#parsed' do
+    it 'returns parsed content of file' do
+      expect(file.parsed).to eq('foo' => 'bar',
+                                'development' => { 'aws' => { 'secret_key' => 12345 }, 'log_level' => 'debug' },
+                                'test' => { 'log_level' => 'fatal' })
+    end
+  end
+
+  describe '#[]' do
+    context 'with no prefix' do
+      it 'returns value for key' do
+        expect(file['foo']).to eq 'bar'
+      end
+    end
+
+    context 'with prefix' do
+      let(:prefix) { 'development' }
+
+      it 'returns value for key' do
+        expect(file['log_level']).to eq 'debug'
+      end
+
+      context 'with nested key' do
+        it 'returns value for key' do
+          expect(file['aws.secret_key']).to eq 12345
+        end
+      end
+    end
+  end
+end
--- spec/conifer_spec.rb
+++ spec/conifer_spec.rb
@@ -4,4 +4,70 @@ RSpec.describe Conifer do
   it 'has a version number' do
     expect(Conifer::VERSION).not_to be nil
   end
+
+  describe '::conifer' do
+    after do
+      Object.send :remove_const, :Model
+    end
+
+    context 'with default parameters' do
+      before do
+        Model = Class.new do
+          include Conifer
+
+          conifer :bar
+        end
+      end
+
+      it 'creates a method with the same name' do
+        expect(Model.new.bar).to be_a Conifer::File
+      end
+    end
+
+    context 'with overridden directory' do
+      before do
+        Model = Class.new do
+          include Conifer
+
+          conifer :foo, dir: File.expand_path('./config', __dir__)
+        end
+      end
+
+      it 'finds correct file' do
+        expect(Model.new.foo.path).to match 'spec/config/foo.yml'
+      end
+    end
+
+    context 'with overridden method name' do
+      before do
+        Model = Class.new do
+          include Conifer
+
+          conifer :bar, method: :foo
+        end
+      end
+
+      it 'creates method with overridden name' do
+        expect(Model.new.foo).to be_a Conifer::File
+      end
+
+      it 'raises error for default method name' do
+        expect { Model.new.bar }.to raise_error NoMethodError
+      end
+    end
+
+    context 'when singleton is true' do
+      before do
+        Model = Module.new do
+          include Conifer
+
+          conifer :bar, singleton: true
+        end
+      end
+
+      it 'defines method at class level' do
+        expect(Model.bar).to be_a Conifer::File
+      end
+    end
+  end
 end
